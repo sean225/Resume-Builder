a791cdbe9ef7b172cd5da85ff7ded2e717baec7e
seanna <122144626+sean225@users.noreply.github.com>
2024-04-25 05:31:29 +0000

    Initial commit

---
+++ src/Data/data.js
@@ -0,0 +1,38 @@
+export const data = {
+  personal_info: {
+    first_name: "Seanna",
+    last_name: "Sam",
+    email: "seannasam374@gmail.com",
+    mobile: "+919972308230",
+    address: "KS Layout, Bangalore",
+    city: " Bangalore",
+    state: "Karnataka",
+    postal_code: "560078",
+    objective:
+      "Front-End Developer with experience in React.js, Redux, and REST APIs, as well as a working knowledge of Node.js, a popular back-end language. I am passionate about creating dynamic, interactive web applications that provide an engaging user experience.",
+  },
+  work_experience: [
+    {
+      id: 1,
+      job_title: "Web 2 & Web 3 Developer",
+      organization_name: "Alma Better",
+      start_year: "2023",
+      end_year: "present",
+    },
+    {
+      id: 2,
+      job_title: "Web 2 & Web 3 Developer",
+      organization_name: "Alma Better",
+      start_year: "2023",
+      end_year: "present",
+    },
+  ],
+  education_details: {
+    type: "Graduation",
+    university: "Bangalore",
+    degree: "BSC Science",
+    start_year: "2023",
+    end_year: "2024",
+  },
+  key_skills: ["ReactJs", "Javascript", "Node Js", "Redux"],
+};
